596dfc67577e476370711af803834d88ec844cff
callmegopi <gopi00076@gmail.com>
2023-03-15 10:17:00 +0530

    class-94

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { DistrictDetailes } from "./DistrictDetailes";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="py-2 bg-light border-bottom">
+        <div className="container d-flex flex-wrap">
+          <ul className="nav me-auto">
+            
+          </ul>
+          <ul className="nav">
+            <li className="nav-item">
+              <a href="#" className="nav-link link-dark px-2">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link link-dark px-2">
+                Sign up
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <DistrictDetailes/>
     </div>
   );
 }
---
+++ src/DistrictDetailes.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+
+export const DistrictDetailes = () => {
+  const [userInput, setuserInput] = useState("");
+  // const [searchDist, setsearchDist] = useState({});
+
+  const searchDistrict = () => {
+    console.log(userInput);
+    axios.get("http://localhost:3000/districts").then((res) => {
+      console.log(res.data);
+      let dist = res.data.find((d) => d.constituencies.indexOf(userInput) > -1);
+      console.log(dist)
+      // setsearchDist(dist);
+    });
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <form>
+              <div className="mb-3">
+                <label className="form-label">
+                  Enter Assembly constituency
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  value={userInput}
+                  onChange={(e) => {
+                    setuserInput(e.target.value);
+                  }}
+                />
+              </div>
+
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={searchDistrict}
+              >
+                Find District
+              </button>
+            </form>
+          </div>
+          {/* {searchDist.districtName && (
+            <div className="col">
+              <h2>Your New District Name is : {searchDist.districtName}</h2>
+            </div>
+          )} */}
+        </div>
+      </div>
+    </div>
+  );
+};
